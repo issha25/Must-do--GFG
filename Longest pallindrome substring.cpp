c79674e82b6a677b07daf23abf07d2279fac0a45
issha25 <68890096+issha25@users.noreply.github.com>
2020-10-04 09:06:35 +0530

    Create Longest pallindrome substring.cpp

---
+++ Longest pallindrome substring.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        int n=s.length();
+        bool dp[n+1][n+1];
+        int x,y,max=INT_MIN;
+        memset(dp,false,sizeof(dp));
+        for(int i=n-1; i>=0 ;i--){
+            for(int j=i; j<=n-1 ;j++){
+                
+                if(i==j)
+                    dp[i][j]=true;
+                
+                else if(s[i]==s[j]){
+                    if(j-i==1)  dp[i][j]=true; 
+                    
+                    else
+                        dp[i][j]= dp[i+1][j-1];
+                    }
+                
+                if(dp[i][j] && j-i>=max){
+                    max=j-i;
+                    x=i;
+                    y=j;
+                 }
+                
+            }
+        }
+        cout<<s.substr(x,y-x+1)<<endl;
+    }
+	//code
+	return 0;
+}
